4d9967bb813c6f507a462ddf165cf7209972109c
Shrinil Thakkar <36529503+shrinil-thakkar@users.noreply.github.com>
2018-04-05 15:08:30 +0530

    Update convert_ip_to_qr.py

--- convert_ip_to_qr.py
+++ convert_ip_to_qr.py
@@ -1,6 +1,10 @@
+"""
+  These commands convert your public ip address to QR code and outputs it on the screen.
+"""
+
 import pyqrcode
 
-# url = <enter url>
+# url = <enter the public you created>
 
 url = pyqrcode.create(public_url)
 print(url.terminal(quiet_zone=1)) 
